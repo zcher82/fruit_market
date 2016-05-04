8e70281b42fc867e96ec222550177a43491b2b4c
zeng her <zcher82@gmail.com>
2016-05-04 16:44:05 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function () {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (1 + max - min) + min);
+}
